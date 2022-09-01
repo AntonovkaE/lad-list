c60e3c8f8e15d434018912b56b338c9563bf51ec
Elvira <sabirova.elvira.gam@gmail.com>
2022-09-01 13:28:56 +0500

    feat: add form

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import Home from "../pages/home";
 import About from "../pages/About";
 import { Navbar } from "./Navbar";
+import { Form } from "./Form";
 
 function App() {
     return (
         <div className="page">
             <Navbar />
-            <div className="container">
+            <div className="container pt-5">
+                <Form/>
                 <Routes>
                     <Route path="/" exact element={ <Home/> }>
                     </Route>
