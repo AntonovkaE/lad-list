531b5d8a7132b5f09f7d2595d69504e00fc629ee
Elvira <sabirova.elvira.gam@gmail.com>
2022-09-01 14:59:16 +0500

    feat: add store with REDUX

---
+++ src/context/alert/AlertState.js
@@ -0,0 +1,8 @@
+import {createContext} from "react";
+import React from "react";
+import { AlertContext } from "./alertContext";
+export const AlertState = ({children}) => {
+    return (
+        <AlertContext/>
+    )
+}
