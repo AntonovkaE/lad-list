16b64fc4ef1f90f136d3f7f26b18878e7675a108
Elvira <sabirova.elvira.gam@gmail.com>
2022-09-10 21:37:50 +0500

    Updates

--- src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export const Footer = () => {
-  const date = new Date().getFullYear()
-  return (
-    <footer className="footer">
-      <p className="footer__copyright">&copy;&nbsp;{ `${ date } Эльвира Антоновская` }</p>
-    </footer>
-  )
-}
--- src/components/Slider/SliderItem/SliderItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const SliderItem = ({ children, width, background }) => {
-  return (
-    <div className="slider__item" style={ { width: width, backgroundImage: background } }>
-      { children }
-    </div>
-  );
-};
